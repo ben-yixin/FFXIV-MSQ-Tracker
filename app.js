c5c833192137cc53ad0219f685161ea8acb39bf0
jusuuu <ben.zhang2124@gmail.com>
2021-08-26 20:16:56 +1200

    Added search function and updated quest database

--- app.js
+++ app.js
@@ -8,39 +8,58 @@ var quest = [];
 fetch('./Quests/questChains.json')
     .then(response => response.json())
     .then(data=>{
-        console.log(data.gridania);
-        quest = data.gridania;
+        //Fill quest variable with all quests in the file
+        quest = data.realmReborn[1]
+        .concat(data.heavensWard)
+        .concat(data.stormBlood)
+        .concat(data.shadowBringers)
+        .concat(data.endWalker);
+        //create an unordered list and href
         ul = document.createElement('ul');
+        a = document.createElement('a');
+        //Append unordered list to div
         document.getElementById('questList').appendChild(ul)
-        data.gridania.forEach(element => {
+        quest.forEach(element => {
+            //create unique href tag for each quest
+            a.href = "#"+element;
+            //create a new list element for each quest
             let li = document.createElement('li');
-            ul.appendChild(li);
+            //append href tag to new list element
+            li.appendChild(a);
+            //run select function on click
+            li.addEventListener('click',function(){select(li)});
+            //add text to list item and append to group
             li.innerHTML += element;
+            ul.appendChild(li);
         });
-       // document.getElementById('questList').innerHTML = data.gridania;
     })
 
 
-
-
-function test() {
-   var arr= quest;
+function getProgress(index) {
+    var currentArc;
     var barCurrent = document.getElementById("progressBarCurrent");
     var barTotal = document.getElementById("progressBarTotal");
     var widthCurrent = 0;
     var widthTotal = 0;
-    var percentCurrent = percentage(arr.indexOf(arr[15]),24);
-    var percentTotal = percentage(arr.indexOf(arr[24]),24);
+    var percentCurrent = percentage(index,quest.length-1);
+    var percentTotal = percentage(index,quest.length-1);
     setInterval(sceneCurrent,10);
     setInterval(sceneTotal, 10);
-    var checkOne = false;
-    var checkTwo = false;
+    //Check if user selected the first quest
+    if(percentTotal == 0){
+        barTotal.style.width = 0+'%';
+        barCurrent.style.width = 0+'%'
+        document.getElementById("questTitle").innerHTML = `${quest[index]}`
+        document.getElementById("percentageTotal").innerHTML = `${percentTotal}% through the MSQ!`;
+    }
     function sceneCurrent(){
         if(widthCurrent < percentCurrent){
             widthCurrent++;
             barCurrent.style.width = widthCurrent+'%';
             if(widthCurrent == Math.round(percentCurrent)){ 
-                document.getElementById("percentageCurrent").innerHTML = `You are ${percentCurrent}% through {CurrentArc}!`;
+                document.getElementById("questTitle").innerHTML = `${quest[index]}`
+                document.getElementById("percentageCurrent").innerHTML = `You are ${percentCurrent}% through {currentArc}!`;
+                quest[index]
             }
         }
     }
@@ -48,15 +67,11 @@ function test() {
         if(widthTotal < percentTotal){
             widthTotal++;
             barTotal.style.width = widthTotal+'%';
-            if(widthTotal == Math.round(percentTotal)){
-                document.getElementById("percentageTotal").innerHTML = `You are ${percentTotal}% through the MSQ!`;
-            }
+            if(widthTotal == Math.round(percentTotal) || widthTotal == Math.ceil(percentTotal)){
+                document.getElementById("questTitle").innerHTML = `${quest[index]}`
+                document.getElementById("percentageTotal").innerHTML = `${percentTotal}% through the MSQ!`;
+            } 
         }
-    }
-    console.log(checkOne)
-    if(checkOne === true && checkTwo === true){
-        document.getElementById("percentageCurrent").innerHTML = `You are ${percentCurrent}% through {CurrentArc}!`;
-        document.getElementById("percentageTotal").innerHTML = `You are ${percentTotal}% through the MSQ!`;
     }
 }
 /*
@@ -72,4 +87,37 @@ function scene(width,percent,element,id){
 */
 function percentage(x,y){
     return ((x * 100)/y).toFixed(2);
+}
+function search() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("questList");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      //a = li[i].getElementsByTagName("li")[0];
+      //txtValue = a.textContent || a.innerText;
+     
+      txtValue = li[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+}
+
+//Clicking on item will autocomplete the search bar
+function select(input){
+    document.getElementById("myInput").value = input.innerText;
+}
+//Sending the form will run getProgress
+function enter(){
+    var input = document.getElementById("myInput").value;
+    //search quests for index of item
+    var index = quest.indexOf(input);
+    getProgress(index)
 }
